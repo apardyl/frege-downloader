e9eaf6c816b387efb5bd8e06c9a75cc05dac4632
Piotr Mucha <piotrmucha1997@gmail.com>
2021-01-21 23:49:13 +0100

    remove tests

--- src/main/java/com/piotrekapplications/downloader/DownloaderApplication.java
+++ src/main/java/com/piotrekapplications/downloader/DownloaderApplication.java
@@ -10,27 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.sql.Timestamp;
 
 @SpringBootApplication
-public class DownloaderApplication implements CommandLineRunner {
-    @Autowired
-    private RepositoryRepo database;
+public class DownloaderApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(DownloaderApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Repositories repositories = new Repositories("some_id","https://github.com/Software-Engineering-Jagiellonian/frege-indexer-lib.git",
-                "repo_url",new Timestamp(System.currentTimeMillis()), null, null);
-        database.save(repositories);
-        Repositories repositories2 = new Repositories("some_id2","https://github.com/Software-Engineering-Jagiellonian/frege-indexer-lib.git",
-                "repo_url",new Timestamp(System.currentTimeMillis()), null, null);
-        database.save(repositories2);
-        Repositories repositories3 = new Repositories("some_id3","https://github.com/Software-Engineering-Jagiellonian/frege-indexer-lib.git",
-                "repo_url",new Timestamp(System.currentTimeMillis()), null, null);
-        database.save(repositories3);
-        Repositories repositories4 = new Repositories("some_id4","https://github.com/Software-Engineering-Jagiellonian/frege-indexer-lib.git",
-                "repo_url",new Timestamp(System.currentTimeMillis()), null, null);
-        database.save(repositories4);
-    }
+
 }
